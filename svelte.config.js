af930b5b38d660ea6bc0970f6e41b9241a74ae9f
PBara7a <pbara7a@gmail.com>
2025-10-18 16:55:57 +0100

    setup tailwind and prettier + pre-commit hook

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  base: "/sparta-roster-manager/",
+  base: '/sparta-roster-manager/',
 };
